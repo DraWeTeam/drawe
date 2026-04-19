728ed222276a45d4bd003e730c15f59a5f6b2984
YujinB <yujinb0201@gmail.com>
2026-04-20 02:14:08 +0900

    SCRUM-31 feat: 로그인 API 프론트-백엔드 연동 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login/Login";
 import Template from "./template/Template";
+import Login from "./pages/login/Login";
+import OAuthCallback from "./pages/login/OAuthCallback";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
         <Template>
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/oauth/callback" element={<OAuthCallback />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </Template>
     </BrowserRouter>
---
+++ src/pages/login/api.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url?.includes("/auth/refresh")
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        if (!refreshToken) {
+          throw new Error("refreshToken 없음");
+        }
+
+        const response = await axios.post("http://localhost:8080/auth/refresh", {
+          refreshToken,
+        });
+
+        const newAccessToken = response.data.data.accessToken;
+        const newRefreshToken = response.data.data.refreshToken;
+
+        localStorage.setItem("accessToken", newAccessToken);
+        localStorage.setItem("refreshToken", newRefreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(originalRequest);        
+      } catch (refreshError) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
