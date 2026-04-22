b641a7376def21a76cb48f9737236a16ad80c4c4
hkseo <harp12553@knou.ac.kr>
2026-04-22 11:56:42 +0900

    feat: add docker setup (Dockerfile, docker-compose)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Template from "./template/Template";
 import Login from "./pages/login/Login";
 import OAuthCallback from "./pages/login/OAuthCallback";
 import Home from "./pages/Home";
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("accessToken");
+  return token ? children : <Navigate to="/login" replace />;
+};
 
 function App() {
   return (
@@ -11,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/oauth/callback" element={<OAuthCallback />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
         </Routes>
       </Template>
     </BrowserRouter>
