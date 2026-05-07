451bce8d9cf5db263f3c256344c5b1b2a7a5730a
hkseo <harp12553@knou.ac.kr>
2026-05-07 20:26:42 +0900

    Redis Repository 자동설정 제외

--- src/main/java/com/drawe/backend/DraweBackendApplication.java
+++ src/main/java/com/drawe/backend/DraweBackendApplication.java
@@ -3,8 +3,9 @@ package com.drawe.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { RedisRepositoriesAutoConfiguration.class })
 @EnableAsync
 public class DraweBackendApplication {
 
